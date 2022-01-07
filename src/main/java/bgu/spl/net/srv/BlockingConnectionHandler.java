75dddecd5f664e9ee805dcebba2347690a35dbae
shoham cohen <shohco@post.bgu.ac.il>
2022-01-07 15:37:54 +0200

    "add changes to both connectionHandler"

--- src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -9,17 +9,21 @@ import java.net.Socket;
 
 public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
 
-    private final MessagingProtocol<T> protocol;
+    private final BidiMessagingProtocol<T> protocol;
     private final MessageEncoderDecoder<T> encdec;
     private final Socket sock;
     private BufferedInputStream in;
     private BufferedOutputStream out;
     private volatile boolean connected = true;
+    private int ConnectionId;
 
-    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, MessagingProtocol<T> protocol) {
+    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, BidiMessagingProtocol<T> protocol,int connectionId) {
         this.sock = sock;
         this.encdec = reader;
         this.protocol = protocol;
+        ConnectionId=connectionId;
+        protocol.start(connectionId,new ConnectionsImp(connectionId));
+
     }
 
     @Override
@@ -33,11 +37,8 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
             while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
                 T nextMessage = encdec.decodeNextByte((byte) read);
                 if (nextMessage != null) {
-                    T response = protocol.process(nextMessage);
-                    if (response != null) {
-                        out.write(encdec.encode(response));
-                        out.flush();
-                    }
+                    protocol.process(nextMessage);
+
                 }
             }
 
